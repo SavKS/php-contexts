e8980aba2ea9786adbcab081b5877aa47fdb5b1a
Andrii Savluk <a.savluk@ideil.com>
2023-08-01 10:45:50 +0300

    v1.2.0

--- src/Context.php
+++ src/Context.php
@@ -12,26 +12,34 @@ abstract class Context
         return (fn () => $callback())();
     }
 
-    public static function tryUseSelf(): ?static
+    public static function tryUseSelf(bool $withInherited = false): ?static
     {
-        return static::tryUse(static::class);
+        // @phpstan-ignore-next-line
+        return static::tryUse(static::class, $withInherited);
     }
 
-    public static function useSelf(): static
+    public static function useSelf(bool $withInherited = false): static
     {
-        return static::use(static::class);
+        // @phpstan-ignore-next-line
+        return static::use(static::class, $withInherited);
     }
 
     /**
      * @param class-string<TContext> $contextFQN
      * @return TContext|null
      */
-    public static function tryUse(string $contextFQN): ?Context
+    public static function tryUse(string $contextFQN, bool $withInherited = false): ?Context
     {
-        $backtrace = \debug_backtrace();
+        $backtrace = debug_backtrace();
 
         foreach ($backtrace as $step) {
-            if (isset($step['object']) && $step['object']::class === $contextFQN) {
+            if (! isset($step['object'])) {
+                continue;
+            }
+
+            if ($withInherited && is_subclass_of($step['object']::class, $contextFQN)) {
+                return $step['object'];
+            } elseif ($step['object']::class === $contextFQN) {
                 return $step['object'];
             }
         }
@@ -43,9 +51,9 @@ abstract class Context
      * @param class-string<TContext> $contextFQN
      * @return TContext
      */
-    public static function use(string $contextFQN): Context
+    public static function use(string $contextFQN, bool $withInherited = false): Context
     {
-        $context = static::tryUse($contextFQN);
+        $context = static::tryUse($contextFQN, $withInherited);
 
         if (! $context) {
             throw new ContextNotFound($contextFQN);
